98d2c98dd7aba8c025b2d299dfdcab180b685cad
aziel <2021-0165@unad.edu.do>
2025-07-21 13:18:50 -0400

    invetario farmacia 0.3

--- Productos.cs
+++ Productos.cs
@@ -19,6 +19,8 @@ namespace Inventario_de_farmacia
 
         private void Productos_Load(object sender, EventArgs e)
         {
+            // TODO: esta línea de código carga datos en la tabla 'farmacia2DataSet.PRODUCTOS' Puede moverla o quitarla según sea necesario.
+            this.pRODUCTOSTableAdapter.Fill(this.farmacia2DataSet.PRODUCTOS);
             this.FormClosing += new FormClosingEventHandler(Cerrar);
             this.Visible = false;
             this.StartPosition = FormStartPosition.CenterScreen;
